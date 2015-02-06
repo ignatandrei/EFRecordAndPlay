3d032246c263beb5e6c638d2ae8ce4436560e324
Andrei Ignat <ignatandrei@yahoo.com>
2015-02-06 08:39:09 +0100

    Create github.jquery.js

---
+++ javascripts/github.jquery.js
@@ -0,0 +1,29 @@
+/ http://aboutcode.net/2010/11/11/list-github-projects-using-javascript.html
+ 
+jQuery.githubUser = function(username, callback) {
+  //jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
+}
+ 
+jQuery.fn.loadRepositores = function(username) {
+  this.html("<span>Querying GitHub for repositories...</span>");
+ 
+  var target = this; 
+  $.githubUser(username, function(data) {
+    var repos = data.user.repositories;
+    sortByNumberOfWatchers(repos);
+ 
+    var list = $('<dl/>');
+    target.empty().append(list);
+    $(repos).each(function() {
+      list.append('<dt><a href="'+ this.url +'">' + this.name + '</a></dt>');
+      list.append('<dd>' + this.description + '</dd>');
+    });
+  });
+ 
+  function sortByNumberOfWatchers(repos) {
+    repos.sort(function(a,b) {
+      return b.watchers - a.watchers;
+    });
+  }
+};
